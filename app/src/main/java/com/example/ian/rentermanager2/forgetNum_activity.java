3eef9a55f045a623735f67506e488e91036e807e
lan888 <414545882@qq.com>
2017-12-07 22:19:20 +0800

    bmob demo

--- app/src/main/java/com/example/ian/rentermanager2/forgetNum_activity.java
+++ app/src/main/java/com/example/ian/rentermanager2/forgetNum_activity.java
@@ -1,20 +1,17 @@
 package com.example.ian.rentermanager2;
 
-import android.content.DialogInterface;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import static com.example.ian.rentermanager2.R.id.pass;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
 
 /**
  * Created by Ian on 2017/8/3 0003.
@@ -22,17 +19,13 @@ import static com.example.ian.rentermanager2.R.id.pass;
 
 public class forgetNum_activity extends AppCompatActivity {
 
-    private EditText name;
     private EditText code;
     private Button find;
-    private myDatabaseHelper dbHelper;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgetnum_layout);
 
-        dbHelper=myDatabaseHelper.getInstance(this);
-        name = (EditText) findViewById(R.id.admin_register_name1);
         code = (EditText) findViewById(R.id.admin_register_info1);
         find = (Button) findViewById(R.id.find);
 
@@ -40,41 +33,23 @@ public class forgetNum_activity extends AppCompatActivity {
         find.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nameInfo = name.getText().toString();
-                String codeInfo = code.getText().toString();
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                Cursor cursor = db.rawQuery("select name from admin where name=? ",new String[]{nameInfo});
-                Cursor cursor1 = db.rawQuery("select password from admin where name=?",new String[]{nameInfo});
-                if (cursor.moveToNext()){
-                    if (codeInfo.equals("10086")){
-                        AlertDialog.Builder builder = new AlertDialog.Builder(forgetNum_activity.this);
-                        LayoutInflater factory = LayoutInflater.from(forgetNum_activity.this);
-                         View textEntryView = factory.inflate(R.layout.password,null);
-                        builder.setView(textEntryView);
-                         TextView password1 =  textEntryView.findViewById(pass);
 
-                        if (cursor1.moveToNext()){
+               final String codeInfo = code.getText().toString();
 
-                           String pi= cursor1.getString(cursor1.getColumnIndex("password"));
-                            password1.setText(pi);
+                BmobUser.resetPasswordByEmail(codeInfo, new UpdateListener() {
 
-
+                    @Override
+                    public void done(BmobException e) {
+                        if(e==null){
+                            Toast.makeText(forgetNum_activity.this,"重置密码请求成功，请到" + codeInfo + "邮箱进行密码重置操作",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(forgetNum_activity.this,MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(forgetNum_activity.this,"失败:" + e.getMessage(),Toast.LENGTH_SHORT).show();
                         }
-
-
-                        builder.setNegativeButton("确定", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-
-                            }
-                        });
-                        builder.create().show();
-                    }else {
-                        Toast.makeText(forgetNum_activity.this,"注册码不正确",Toast.LENGTH_SHORT).show();
                     }
-                }else {
-                    Toast.makeText(forgetNum_activity.this,"管理员不存在",Toast.LENGTH_SHORT).show();
-                }
+                });
+
             }
         });
 
